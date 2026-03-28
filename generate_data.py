14df375ee765d20aa62183cc948bded427bf6e9f
root <root@cv6245091.novalocal>
2026-03-28 19:27:56 +0300

    Исправление ошибок

--- generate_data.py
+++ generate_data.py
@@ -3,9 +3,18 @@ import random
 import string
 import os
 from datetime import datetime
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+logging.basicConfig(filename=f"/home/Auto_fin/logs/generator.log", level=logging.DEBUG)
+
+
+LOGGER.debug(f"\n\n\n\n\nstart new launch: {datetime.now()}")
+
 
 #создание папки дата, если она не существует
-data_folder = 'data'
+data_folder = '/home/Auto_fin/data'
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 
@@ -33,14 +42,17 @@ def generate_unique_doc_id(existing_ids):
         doc_id = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
         if doc_id not in existing_ids:
             break
+    LOGGER.debug(f"was created new uniq doc id: {doc_id}")
     existing_ids.add(doc_id)
     return doc_id
 
 today = datetime.today()
 #цикл для создания csv файлов по каждому магазину и кассе
-res_data = []
+
+full_res_data = []
 for shop, num_cashes in shops_cashes.items():
     for cash in range(1, num_cashes + 1):
+        res_data = []
         num_items = random.randint(1, 5)  # количество разных товаров в чеке
         for _ in range(num_items):
             category = random.choice(categories)
@@ -49,7 +61,7 @@ for shop, num_cashes in shops_cashes.items():
             price = random.uniform(10, 130)
             discount = random.randint(0, 30)
             doc_id = generate_unique_doc_id(existing_doc_ids)
-            res_data.append({
+            new_order = {
                 'date': today.strftime("%Y-%m-%d"),
                 'shop': shop,
                 'cash': cash,
@@ -59,12 +71,11 @@ for shop, num_cashes in shops_cashes.items():
                 'amount': amount,
                 'price': round(price, 2),
                 'discount': round(discount, 2),
-            })
+            }
+            LOGGER.debug(f"was created new order: {new_order}")
+            res_data.append(new_order)
 
         df = pd.DataFrame(res_data) # Конвертация данных в DataFrame
         df.to_csv(os.path.join(data_folder, f'{shop}_{cash}.csv'), index=False, encoding='utf-8') # Сохранение в файл CSV
-
-
-
-
+        full_res_data.append(res_data)
 
--- run.py
+++ run.py
@@ -2,6 +2,17 @@ import configparser
 import os
 import pandas as pd
 from db import PGDatabase
+import logging
+import traceback
+from datetime import datetime
+
+LOGGER = logging.getLogger(__name__)
+
+logging.basicConfig(filename="/home/Auto_fin/logs/db_writer.log", level=logging.DEBUG)
+
+LOGGER.debug(f"\n\n\n\n\nLaunch script. {datetime.now()}")
+
+data_folder_path: str = "/home/Auto_fin/data"
 
 config = configparser.ConfigParser()
 dirname = os.path.dirname(__file__)
@@ -16,20 +27,27 @@ database = PGDatabase(
     password=DATABASE_CREDS["PASSWORD"],
 )
 
-sales_df = pd.DataFrame()
-if os.path.exists('data'):
-    for root, dirs, files in os.walk('data'):
+
+if os.path.exists(data_folder_path):
+    for root, dirs, files in os.walk(data_folder_path):
+        LOGGER.debug(f"root: {root}, dirs: {dirs}, files: {files}")
         for file in files:
             file_path = os.path.join(root, file)
             sales_df = pd.read_csv(file_path)
             for i, row in sales_df.iterrows():
                 try:
+                    LOGGER.debug(f"INSTERING into database next row: {row}")
                     query = f"insert into sales values ('{row['date']}', {row['shop']}, {row['cash']}, '{row['doc_id']}', '{row['item']}', '{row['category']}', {row['amount']}, {row['price']}, {row['discount']})"
                     database.post(query)
+                    LOGGER.debug("row was insterted")
                 except Exception as e:
+                    LOGGER.debug(f"Insterting failed: {e}, {traceback.format_exc()}")
                     print(f"Ошибка при обработке файла {file}: {e}")
             try:
+                LOGGER.debug(f"removing file_path: {file_path}")
                 os.remove(file_path)
                 print(f"Файл {file} был успешно удалён.")
+                LOGGER.debug(f"removing was successfully done.")
             except Exception as e:
+                LOGGER.debug(f"failed to remove file: {file_path}")
                 print(f"Ошибка при удалении файла {file_path}: {e}")
